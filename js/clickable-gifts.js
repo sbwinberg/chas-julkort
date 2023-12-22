795b55fb05a58560635ace96836d8e6b1242e85c
Emil <emil.winberg1@gmail.com>
2023-12-22 11:47:34 +0100

    flying elf and other eventlisteners

---
+++ js/clickable-gifts.js
@@ -0,0 +1,51 @@
+const all_gifts = document.querySelector('.gifts');
+const easter = document.querySelector('.easter');
+const egg = document.querySelector('.egg');
+const gift = document.querySelector('.gift');
+const present = document.querySelector('.present');
+const klapp = document.querySelector('.klapp');
+const box = document.querySelector('.box');
+const paket = document.querySelector('.paket');
+const gava = document.querySelector('.gava');
+const lada = document.querySelector('.gifts');
+const burk = document.querySelector('.gifts');
+const kartong = document.querySelector('.gifts');
+const elf = document.querySelector('.elf');
+
+easter.addEventListener('click', () => {
+    console.log('GOD JUL');
+})
+egg.addEventListener('click', () => {
+    elf.style.transform = 'translateX(1000%) rotate(1080deg)'
+    setTimeout(() => {
+        elf.style.transform = 'translateX(-1000%) rotate(-1080deg)'
+    }, 1000);
+    
+})
+gift.addEventListener('click', () => {
+    alert("Hoppas tomten levererar 'packages' av glädje och 'dependencies' av lycka. God jul och en 'commit'-full start på det nya året!")
+})
+present.addEventListener('click', () => {
+    
+})
+klapp.addEventListener('click', () => {
+    
+})
+box.addEventListener('click', () => {
+    
+})
+paket.addEventListener('click', () => {
+    
+})
+gava.addEventListener('click', () => {
+    
+})
+lada.addEventListener('click', () => {
+    
+})
+burk.addEventListener('click', () => {
+    
+})
+kartong.addEventListener('click', () => {
+    
+})
