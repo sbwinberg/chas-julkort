6dd3967897c7eda124e8d0cf0386c4f69ab0b00a
Jens Bengtsson <jb225je@student.lnu.se>
2023-12-22 12:24:25 +0100

    grinch

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// ------------------- grinchen del -------------------
+
+const grinchenFeature = document.querySelector('.grinch-containter');
+const grinchenimg = document.querySelector('.grinch');
+
+grinchenFeature.addEventListener('click', () => {
+    grinchenimg.classList.remove('display-none');
+    grinchenimg.classList.add('start-animation');
+    setTimeout(greeting, 4git 000);
+    console.log('click');
+})
+
+document.addEventListener('keydown', (e) => {
+    //clickGrinch = document.addEventListener('click', )
+    if (e.key === 'Escape') {
+        console.log('esc')
+        grinchenimg.classList.add('display-none');
+        document.querySelector('.greeting1').classList.add('display-none');
+    }
+});
+
+function greeting() {
+    document.querySelector('.greeting1').classList.remove('display-none');
+}
+
+// ------------------- grinchen slut -------------------
